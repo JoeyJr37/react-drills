731ff2a93f1eecf6c3fc35658b0dbcdce6fc5d84
Joey <Joey@antiochhouston.com>
2021-06-23 10:33:00 -0500

    Finished app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Image from './Image.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      activeImage: 'https://i.pinimg.com/736x/b0/81/0f/b0810f05663b8a260401690210085cb4.jpg'
+    }
+  }
+
+  render(){
+    return (
+      <div className='App'>
+        <Image imgUrl={this.state.activeImage} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ app-5/src/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = ({imgUrl}) => {
+    return (
+        <img src={imgUrl} alt='verse from google' />
+    )
+}
+
+export default Image
